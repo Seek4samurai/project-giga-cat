00344f0d47a00888ae71e4eb4d59051afa79eb6d
Seek4samurai <grvrawat999@gmail.com>
2022-08-05 11:53:21 +0530

    chore: added moralis & auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { MoralisProvider } from "react-moralis";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <MoralisProvider
+        serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER_URL}
+        appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
+      >
+        <Component {...pageProps} />
+      </MoralisProvider>
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,37 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useMoralis } from "react-moralis";
 
 export default function Home() {
+  const { isAuthenticated, authenticate, user } = useMoralis();
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    console.log("Login");
+    await authenticate({
+      signingMessage: "Authorize linking of your wallet",
+    })
+      .then(function (user) {
+        console.log(user.get("ethAddress"));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      console.log();
+      router.replace("/dashboard");
+    }
+  }, [isAuthenticated, router]);
+
   return (
-    <div>
-      <Link href="/game">
-        <a>Start Game!</a>
-      </Link>
-    </div>
+    <>
+      <div>
+        <button onClick={handleLogin}>Login</button>
+      </div>
+    </>
   );
 }
