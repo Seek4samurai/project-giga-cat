d4b9b530bd11d45001c9d2f07778811a8ca2627e
Seek4samurai <grvrawat999@gmail.com>
2022-08-09 00:37:04 +0530

    style: landing login page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useMoralis } from "react-moralis";
+import { Github, LinkedIn, SVG_1, SVG_2 } from "../public/assets/SVGs";
+import style from "../styles/Login.module.css";
 
 export default function Home() {
   const { isAuthenticated, authenticate, user } = useMoralis();
@@ -30,10 +32,48 @@ export default function Home() {
   }, [isAuthenticated, router]);
 
   return (
-    <>
-      <div>
-        <button onClick={handleLogin}>Login</button>
+    <div className={style.body}>
+      <div className={style.container}>
+        <div className={style.SVGContainer}>
+          <div className={style.SVG_1}>
+            <SVG_1></SVG_1>
+          </div>
+          <div className={style.SVG_2}>
+            <SVG_2></SVG_2>
+          </div>
+        </div>
+        <div className={style.TopBarContainer}>
+          <div className={style.MeetCreatorContainer}>
+            <div className={style.LinkedIn}>
+              <LinkedIn></LinkedIn>
+            </div>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/seek4samurai/"
+              style={{ cursor: "pointer" }}
+            >
+              <h3 style={{ textDecoration: "underline" }}>Meet Creator!</h3>
+            </a>
+          </div>
+          <div className={style.Contribute}>
+            <div className={style.Github}>
+              <Github></Github>
+            </div>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/Seek4samurai/project-giga-cat"
+              style={{ cursor: "pointer" }}
+            >
+              <h3 style={{ textDecoration: "underline" }}>Contribute!</h3>
+            </a>
+          </div>
+        </div>
+        {/* <div>
+          <button onClick={handleLogin}>Connect Wallet</button>
+        </div> */}
       </div>
-    </>
+    </div>
   );
 }
---
+++ public/assets/SVGs.js
@@ -0,0 +1,99 @@
+export const Github = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 496 512"
+    fill="#eee"
+    {...props}
+  >
+    <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+  </svg>
+);
+
+export const LinkedIn = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 448 512"
+    fill="#eee"
+    {...props}
+  >
+    <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
+  </svg>
+);
+
+export const SVG_1 = (props) => (
+  <svg
+    width={1272}
+    height={577}
+    fill="#FFA621"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M1208.36 211.289C947.689 377.112 539.301 539.378 172.871 575.547 77.087 585.002 0 505.771 0 409.521V0h1127c139.99 0 199.48 136.149 81.36 211.289Z"
+      fill="#FFA621"
+    />
+    <defs>
+      <filter
+        id="a"
+        x={-4}
+        y={-3}
+        width={1275.45}
+        height={579.315}
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+        <feColorMatrix
+          in="SourceAlpha"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dx={-7} dy={-3} />
+        <feGaussianBlur stdDeviation={2} />
+        <feComposite in2="hardAlpha" operator="arithmetic" k2={-1} k3={1} />
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend in2="shape" result="effect1_innerShadow_4_3" />
+      </filter>
+    </defs>
+  </svg>
+);
+
+export const SVG_2 = (props) => (
+  <svg
+    width={688}
+    height={367}
+    fill="#FFA621"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M65.793 171.824C195.416 97.447 359.06 31.567 516.167 3.028 610.718-14.148 688 64.895 688 160.994V367H139.178C2.568 367-52.697 239.813 65.793 171.824Z"
+      fill="#"
+    />
+    <defs>
+      <filter
+        id="a"
+        x={-3.086}
+        y={-2.332}
+        width={691.086}
+        height={369.332}
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+        <feColorMatrix
+          in="SourceAlpha"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dx={-7} dy={-3} />
+        <feGaussianBlur stdDeviation={2} />
+        <feComposite in2="hardAlpha" operator="arithmetic" k2={-1} k3={1} />
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend in2="shape" result="effect1_innerShadow_4_28" />
+      </filter>
+    </defs>
+  </svg>
+);
