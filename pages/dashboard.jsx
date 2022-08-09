405869aa1fc65990d66d492ce3854a37cae61943
Seek4samurai <grvrawat999@gmail.com>
2022-08-09 13:10:32 +0530

    style: Dashboard page complete

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useMoralis } from "react-moralis";
+import { User } from "../public/assets/SVGs";
+import style from "../styles/Dashboard.module.css";
 
 const Dashboard = () => {
   const { isAuthenticated, user, logout } = useMoralis();
@@ -32,19 +34,46 @@ const Dashboard = () => {
   }, [isAuthenticated, router, user, userAddress]);
 
   return (
-    <div>
-      <h2>
-        Logged in as {userAddress?.slice(0, 6)}...{userAddress?.slice(39)}
-      </h2>
-      <button onClick={handleLogout}>Logout</button>
-      <Link
-        href={{
-          pathname: "/game",
-          query: userAddress,
-        }}
-      >
-        <a>Start Game!</a>
-      </Link>
+    <div className={style.body}>
+      <div className={style.container}>
+        <div className={style.TopBar}>
+          <div className={style.Profile}>
+            <User></User>
+          </div>
+          <h1 className={style.Head}>
+            Welcome {userAddress?.slice(0, 6)}...{userAddress?.slice(39)}
+          </h1>
+        </div>
+        <Link
+          href={{
+            pathname: "/game",
+            query: userAddress,
+          }}
+        >
+          <button className={style.PlayBtn}>
+            <a>PLAY!</a>
+          </button>
+        </Link>
+        <h1 className={style.Head}>How to play</h1>
+        <div className={style.Content}>
+          <p>
+            If you were a real gamer...you would have skipped this part, but
+            anyways use <span>SPACE </span>to dodge obstacles (simple as that).
+          </p>
+          <p>
+            Player with highest run will be shown on the{" "}
+            <span>leaderboard</span>.
+          </p>
+          <p>
+            Scores are erased after every few hours, so make sure to return back
+            & <br></br>
+            <span>hold your grounds</span>.
+          </p>
+        </div>
+        <button className={style.LogoutBtn} onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
     </div>
   );
 };
